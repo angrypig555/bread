405229dcf7dc5a7c67e1764a8a4772106db1be93
angrypig555 <brny@brny.dev>
2026-03-02 17:48:45 +0000

    store filename

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 
+std::string filename;
+
 int main(int argc, char *argv[]) {
-    std::cout << "hello from the bread compiler!" << std::endl;
+    if (argc < 2) {
+        std::cout << "no arguments given, enter filename to compile .bread file" << std::endl;
+        return 1;
+    } else {
+        filename = argv[1];
+    }
+    return 0;
 }
